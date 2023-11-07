1c25579c2bb2aa1feb2f8c53937f973e1ceac6d8
DimasVhigarr <dimasvhigar2002@gmail.com>
2023-11-08 02:03:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktekappesjumbo/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.praktekappesjumbo.data
+
+import com.example.praktekappesjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.leci,
+    )
+}
