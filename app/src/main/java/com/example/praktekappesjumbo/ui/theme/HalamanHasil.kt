00b4c653cd3c2c2965e98155e212b7ce1de00081
DimasVhigarr <dimasvhigar2002@gmail.com>
2023-11-16 01:13:45 +0700

    add fun DataPemesan di FormatUI

--- app/src/main/java/com/example/praktekappesjumbo/ui/theme/HalamanHasil.kt
+++
@@ -1,2 +0,0 @@
-package com.example.praktekappesjumbo.ui.theme
-
--- app/src/main/java/com/example/praktekappesjumbo/ui/theme/komponen/FormatUI.kt
+++ app/src/main/java/com/example/praktekappesjumbo/ui/theme/komponen/FormatUI.kt
@@ -1,10 +1,14 @@
 package com.example.praktekappesjumbo.ui.theme.komponen
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.praktekappesjumbo.R
 
 @Composable
@@ -13,3 +17,32 @@ fun FormatLabelHarga(subtotal: String, modifier: Modifier = Modifier){
         modifier = modifier,
         style = MaterialTheme.typography.headlineSmall)
 }
+
+@Composable
+fun DataPemesan(
+    namaPemesan: String,
+    alamatPemesan: String,
+    telpPemesan: String,
+){
+    Text(
+        text = stringResource(R.string.nama)
+    )
+    Text(namaPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    Text(
+        text = stringResource(R.string.alamat)
+    )
+    Text(alamatPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    Text(
+        text = stringResource(R.string.tlp),
+    )
+    Text(telpPemesan)
+    Divider()
+
+}
+
